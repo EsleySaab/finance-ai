4372b3d72a6b9aae625642839861d916591c171a
EsleySaab <esleysantana.sa@gmail.com>
2025-03-03 18:23:03 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -16,14 +16,14 @@ const LoginPage = async () => {
       {/* ESQUERDA */}
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
         <Image
-          src="/logo.svg"
+          src="/logo-finance.svg"
           alt="Finance AI"
           width={173}
           height={39}
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground mb-8">
+        <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
